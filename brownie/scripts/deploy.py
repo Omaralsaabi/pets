9d205a21df0111d10fbf2bca1c7f38f801572547
Omaralsaabi <prof.omaralsaabi@gmail.com>
2023-07-04 11:59:26 +0300

    modified

--- brownie/scripts/deploy.py
+++ brownie/scripts/deploy.py
@@ -1,23 +1,12 @@
 from brownie import PetRegistry, accounts, network, config, MockV3Aggregator
 from scripts.helpful_scripts import (
     get_account,
-    deploy_mocks,
     LOCAL_BLOCKCHAIN_ENVIRONMENTS,
 )
 
 
 def deploy_pet_registry():
     account = get_account()
-    # pass the price feed address to our smart contract
-    # if network.show_active() not in LOCAL_BLOCKCHAIN_ENVIRONMENTS:
-    #     price_feed_address = config["networks"][network.show_active()][
-    #         "eth_usd_price_feed"
-    #     ]
-    # else:
-    #     # if we are on a local environment, we need to deploy mocks
-    #     deploy_mocks()
-    #     price_feed_address = MockV3Aggregator[-1].address
-
     pet_registry = PetRegistry.deploy(
         {"from": account},
         publish_source=config["networks"][network.show_active()].get("verify"),
--- brownie/scripts/helpful_scripts.py
+++ brownie/scripts/helpful_scripts.py
@@ -19,9 +19,9 @@ def get_account():
         return accounts.add(config["wallets"]["from_key"])
 
 
-def deploy_mocks():
-    print(f"The active network is {network.show_active()}")
-    print("Deploying Mocks...")
-    if len(MockV3Aggregator) <= 0:
-        MockV3Aggregator.deploy(DECIMALS, STARTING_PRICE, {"from": get_account()})
-    print("Mocks deployed!")
+# def deploy_mocks():
+#     print(f"The active network is {network.show_active()}")
+#     print("Deploying Mocks...")
+#     if len(MockV3Aggregator) <= 0:
+#         MockV3Aggregator.deploy(DECIMALS, STARTING_PRICE, {"from": get_account()})
+#     print("Mocks deployed!")
